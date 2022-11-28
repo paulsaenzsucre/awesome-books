d45bac112ca48ba58b3ee8eac7fb16491369d0a7
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-11-29 00:03:06 +0200

    Fix linters

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 let books = [];
 
+function addBook() {
+  const randomId = Math.floor(Math.random() * 100);
+  const element = {
+    id: randomId,
+    title: document.getElementById('title').value,
+    author: document.getElementById('author').value,
+  };
+  books.push(element);
+
+  localStorage.setItem('books', JSON.stringify(books));
+
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+
+  showBook(element);
+}
+
+function removeBook(id) {
+  const newArray = books.filter((book2) => book2.id !== id);
+  localStorage.setItem('books', JSON.stringify(newArray));
+}
+
 function showBook(book) {
   let element;
   const div = document.createElement('div');
@@ -40,24 +62,3 @@ document.getElementById('form').addEventListener('submit', (e) => {
   addBook();
 });
 
-function addBook() {
-  const randomId = Math.floor(Math.random() * 100);
-  const element = {
-    id: randomId,
-    title: document.getElementById('title').value,
-    author: document.getElementById('author').value,
-  };
-  books.push(element);
-
-  localStorage.setItem('books', JSON.stringify(books));
-
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-
-  showBook(element);
-}
-
-function removeBook(id) {
-  const newArray = books.filter((book2) => book2.id !== id);
-  localStorage.setItem('books', JSON.stringify(newArray));
-}
