a0d1f1dccbd71b8d615970b70b4244e05b98387e
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2022-11-28 12:56:37 -0500

    Add showBooks function.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
 var books = [];
 
+function showBook (book) {
+  let element;
+  const div=document.createElement('div');
+  div.setAttribute('id','book'+book.id);
+  element=document.createElement('p');
+  element.appendChild(document.createTextNode(book.title));
+  div.appendChild(element);
+  element=document.createElement('p');
+  element.appendChild(document.createTextNode(book.author));
+  div.appendChild(element);
+  element=document.createElement('button');
+  element.setAttribute('type','button');
+  element.appendChild(document.createTextNode('Remove'));
+  element.addEventListener('click', () => {
+    div.remove(book.id);
+  })
+  div.appendChild(element);
+  element=document.createElement('hr');
+  div.appendChild(element);
+  document.getElementById('book-cont').appendChild(div);
+}
+
 window.addEventListener('load', ()=>{
   books =JSON.parse(localStorage.getItem('books'));
   if(books === null){
     books = [];
   }
+
+  books.forEach(element => {
+    showBook(element);
+  });
 })
 
 document.getElementById('form').addEventListener('submit', (e)=>{
@@ -19,4 +45,8 @@ document.getElementById('form').addEventListener('submit', (e)=>{
   );
 
   localStorage.setItem('books', JSON.stringify(books));
+
+  books.forEach(element => {
+    showBook(element);
+  });
 })
